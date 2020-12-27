4bea264ed700d49fd085179a61f8bd12486d0408
csdevph <dev.ph@gmx.com>
2020-12-27 13:12:46 +0100

    Nouveau fichier pour EtatSession

---
+++ LogoffC/EtatSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogoffC
+{
+    // Classe template
+    abstract class EtatSession
+    {
+        private TimeSpan dureeEtat;
+
+        protected Session sess; // Contexte
+
+        internal TimeSpan DureeEtat { get => dureeEtat; set => dureeEtat = value; }
+
+        internal EtatSession(Session s, int minutes)
+        {
+            sess = s;
+            dureeEtat = new TimeSpan(0, 0, minutes);
+        }
+
+        internal abstract EtatSession EtatSuivant();
+    }
+}
